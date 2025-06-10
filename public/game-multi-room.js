816150572282a97beeea3e401e95d52135b71587
Yin-1022 <kv.huang931022@gmail.com>
2025-06-11 03:05:26 +0800

    multiInGame_fixPreview

--- public/game-multi-room.js
+++ public/game-multi-room.js
@@ -195,6 +195,7 @@ socket.on('updatePlayerList', (players) => {
 socket.on('gameStarted', () => {
   document.getElementById('roomWrapper').style.display = 'none';
   document.getElementById('gameCanvas').style.display = 'block';
+  document.getElementById('scoreBoard').style.display = 'block';
 
   // 可依需求顯示 multiplayer scoreboard
   startMultiplayerGame(); // 你會在 game-multi-itself.js 中實作這個
